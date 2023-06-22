3f0fe33c4b200f7c770c8acc6144eb6bc566005c
Jennifer TER <jennifer.ter@epita.com>
2023-06-22 20:25:09 +0200

    Ongoing listView of all collaborators

--- README.md
+++ README.md
@@ -3,7 +3,8 @@
 Utilier un .env pour les variables d'environnements
 
 Pour l'authentification j'utilise les classes préfaite de django.contrib.auth tel que LoginView ... et la convention 
-Par conséquent, je ne rajoute pas plusieurs choses tel que le template name car j'ai suivi la convention, 
+Par conséquent, je ne rajoute pas plusieurs choses tel que le template name car j'ai suivi la convention,
+
 
     # Old version
     class CustomPasswordResetView(PasswordResetView):
--- annuaire/views.py
+++ annuaire/views.py
@@ -1,6 +1,17 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+
+from users.models import Collaborator
 
 
 # Create your views here.
-class HomeView(TemplateView):
+class HomeView(ListView):
+    model = Collaborator
+    paginate_by = 15
     template_name = 'annuaire/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context["collaborators"] = Collaborator.objects.all()
+        
+        return context
