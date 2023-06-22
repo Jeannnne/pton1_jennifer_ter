9063f89ce79c1e23a9da8fcd8b4efefb99a4ce2f
Jennifer TER <jennifer.ter@epita.com>
2023-06-22 14:12:08 +0200

    Add template for the login fort + logout + reset password with email. Add two button for login and logout (detect automaticly if a user is authentificated).

--- pton1_jennifer_ter/settings.py
+++ pton1_jennifer_ter/settings.py
@@ -141,3 +141,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+LOGIN_REDIRECT_URL = '/admin'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
