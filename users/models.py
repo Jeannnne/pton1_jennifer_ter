20dfff36f0638cbe9bb603cfe5b454bf79f3b70e
Jennifer TER <jennifer.ter@epita.com>
2023-06-22 18:53:13 +0200

    Coding style

--- users/models.py
+++ users/models.py
@@ -10,13 +10,25 @@ from users.validators import validate_date_range
 class Collaborator(AbstractUser):
     # Not required fields
     email = EmailField(blank=True)
-    date_left = DateField(blank=True, null=True, validators=[validate_date_range])
-    current_direction = CharField(blank=True, max_length=100)
+    date_left = DateField(blank=True,
+                          null=True,
+                          validators=[validate_date_range]
+                          )
+    current_direction = CharField(blank=True, max_length=100,
+                                  help_text="Entrez la direction actuelle du collaborateur "
+                                            "(par exemple, Direction des Systèmes d'Information)."
+                                  )
 
     # Required fields
     current_job = CharField(blank=True, max_length=100)
-    phone_number = PhoneNumberField(blank=True, region='FR', help_text="Entrez un numéro de téléphone au format français (par exemple, 0612345678).")
-    service = CharField(blank=True, max_length=100)
+    phone_number = PhoneNumberField(blank=True, region='FR',
+                                    help_text="Entrez un numéro de téléphone au format français "
+                                              "(par exemple, 0612345678)."
+                                    )
+    service = CharField(blank=True, max_length=100,
+                        help_text="Entrez le service du collaborateur "
+                                  "(par exemple, RH pour Service des Ressources Humaines)."
+                        )
 
     # Not editable fields
     date_joined = DateField(auto_now_add=True, validators=[validate_date_range])
