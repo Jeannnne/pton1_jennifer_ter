a29f3e7b2097601d9ad6955a4ae4835bc9cdea4e
Jennifer TER <jennifer.ter@epita.com>
2023-06-22 17:51:52 +0200

    Mark some field not required

--- users/admin.py
+++ users/admin.py
@@ -7,8 +7,8 @@ from users.models import Collaborator
 
 @admin.register(Collaborator)
 class CollaboratorAdmin(admin.ModelAdmin):
-    exclude = ['password']
+    exclude = ['password', 'last_login']
     list_display = ['username', 'email', 'current_job', 'date_joined', 'date_left']
     list_filter = ['date_joined', 'date_left']
     search_fields = ['username', 'email', 'current_job']
-    readonly_fields = ['date_joined',]
+    readonly_fields = ['date_joined']
--- users/models.py
+++ users/models.py
@@ -8,9 +8,9 @@ from users.validators import validate_french_phone_number, validate_date_range
 # Create your models here.
 
 class Collaborator(AbstractUser):
-    current_job = CharField(max_length=100)
-    phone_number = PhoneNumberField(validators=[validate_french_phone_number])
-    email = EmailField(unique=True)
+    current_job = CharField(blank=True, max_length=100)
+    phone_number = PhoneNumberField(blank=True, validators=[validate_french_phone_number])
+    email = EmailField(blank=True)
     date_joined = DateField(auto_now_add=True, validators=[validate_date_range])
     date_left = DateField(blank=True, null=True, validators=[validate_date_range])
     current_direction = CharField(blank=True, max_length=100)
