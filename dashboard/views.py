3f6bede90d02684f6d72d87391dd952341786b3f
Jennifer TER <jennifer.ter@epita.com>
2023-06-24 18:46:37 +0200

    Jai fait le tout pour twitter mais ca ne marche pas je comprends pas pourquoi

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,16 @@
+import requests
+import tweepy
 from django.views.generic import TemplateView
+
+from pton1_jennifer_ter import settings
 
 
 # Create your views here.
 class HomeView(TemplateView):
     template_name = 'dashboard/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+
